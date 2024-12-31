a80b9f92ce0a1587ff5f9235eaaaf9f18ee1ec60
Weiqiu You <weiqiuy@seas.upenn.edu>
2024-12-31 00:42:54 -0500

    Add nice colors

---
+++ assets/colors.py
@@ -0,0 +1,17 @@
+colors = [
+    '#588c7e',
+    '#babca2',
+    '#284e66',
+    '#7f8d8f',
+    '#467f7d',
+    '#acbc8a',
+    '#ddd8ab',
+    '#654755',
+    '#925e64',
+    '#c68c7a',
+    '#ecd189',
+    '#f7e4aa',
+    '#f2b476',
+    '#f9d49c',
+    '#ff5b5c'  #'#db6b5c' - make it more red
+]
